2986da2bbc20dbf99c4b2c71b0fd57d9fdc3649b
seremwen <seremwe.81@gmail.com>
2024-02-26 17:36:04 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
    <>
    <NavBar />
-   <Header /></>
+   <Header />
+   <Footer />
+   </>
   );
 }
 
